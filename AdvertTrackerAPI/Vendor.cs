3da2974ddb838652b97f1858cb29f85e2dafa7bd
Joshua Oladipo <joshuaoladipo92@gmail.com>
2021-09-14 02:08:06 +0100

    Add project files.

---
+++ AdvertTrackerAPI/Vendor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvertTrackerAPI
+{
+    public class Vendor
+    {
+        public int Id { get; set; }
+        public string VendorName { get; set; }
+        public int VisitorCount { get; set; }
+        public DateTime LastVisted { get; set; }
+    }
+}
