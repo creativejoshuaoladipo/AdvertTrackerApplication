9ef1dbba421666cdbba6c5179690822b3e55442d
Joshua Oladipo <joshuaoladipo92@gmail.com>
2021-09-14 02:34:53 +0100

    Added the Models that will be used in creating the Tables for the User Counts

---
+++ AdvertTrackerAPI/Models/RegristeredUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvertTrackerAPI.Models
+{
+    public class RegristeredUser
+    {
+        public int Id { get; set; }
+        public string Guid { get; set; }
+        public DateTime DateCompleted { get; set; }
+    }
+}
